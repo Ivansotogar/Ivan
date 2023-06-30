e853ae053cd01a04f67172029cd6e0d548a7560a
Ivansotogar <97971518+Ivansotogar@users.noreply.github.com>
2023-06-30 20:24:06 +0000

    +, - , *, / de a y b

---
+++ ejercicio3.py
@@ -0,0 +1,9 @@
+a=0
+b=0
+c=0
+a= float(input("Ingresa A: "))
+b= float(input("Ingresa B: "))
+print(f"La suma de a + b es: {(a+b)}") 
+print(f"La multiplicación de a * b es: {(a*b)}")
+print(f"La resta de a - b es: {(a-b)}")
+print(f"La división de a/b es: {(a/b)}")
