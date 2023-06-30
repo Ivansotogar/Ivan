a0f4d8e12355d3145abe1d17e0655476b043743a
Ivansotogar <97971518+Ivansotogar@users.noreply.github.com>
2023-06-30 20:25:16 +0000

    Área y perímetro de rectángulo

---
+++ ejercicio4.py
@@ -0,0 +1,9 @@
+base= 0
+altura=0
+area=0
+perimetro=0
+base=float(input("Ingrese el valor de la base: "))
+altura=float(input("Ingrese el valor de la altura: "))
+area=(base*altura)
+perimetro=(base*2)+(altura*2)
+print(f"El area es: {area} y el perimetro es: {perimetro}")
